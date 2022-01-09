123a7444671ac609d9b74c3da21bcae6958d5b5e
Mohammed Abdelhay <mamado2000@gmail.com>
2022-01-09 23:57:35 +0200

    create api and web-client

---
+++ TrickingLiberary.Api/Controllers/Home.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLiberary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Home: ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
