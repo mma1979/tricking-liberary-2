0aa696cb08e8f16abd8d917865100afd8e6fb079
Mohammed Abdelhay <mma_79@hotmail.com>
2022-01-21 00:42:21 +0200

    L08: Enhance layout

--- web-client/layouts/default.vue
+++ web-client/layouts/default.vue
@@ -1,9 +1,32 @@
 <template>
   <v-app dark>
+    <v-app-bar dense app>
 
+      <v-toolbar-title>Tricking Liberary</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn depressed v-bind="attrs" v-on="on">
+            Upload
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item @click="()=>{}">
+            <v-list-item-title>Trick</v-list-item-title>
+          </v-list-item>
+          <v-list-item >
+            <v-list-item-title>Submission</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
     <v-main>
       <v-container>
-        <Nuxt />
+        <Nuxt/>
       </v-container>
     </v-main>
 
@@ -11,6 +34,5 @@
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
